07107733e56a18a4bbdfc5cd760c879c91cf5140
Tomas Guajardo <tguajardo@gmail.com>
2025-04-15 22:56:43 +0200

    New Personal web

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const encodedEmail = "dGd1YWphcmRvQGdtYWlsLmNvbQ=="; // Base64 encoded email
-    const decodedEmail = atob(encodedEmail);
-    const contactLink = document.getElementById("contact-link");
-    contactLink.href = "mailto:" + decodedEmail;
-});
